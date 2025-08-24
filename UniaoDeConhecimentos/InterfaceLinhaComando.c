692b49da7a310df16531544fb3cf755b3177dc85
brunoconcli <bruno.concli@becomex.com.br>
2025-08-24 14:29:36 -0300

    Primeira versão funcional do menu

--- UniaoDeConhecimentos/InterfaceLinhaComando.c
+++ UniaoDeConhecimentos/InterfaceLinhaComando.c
@@ -1,15 +1,15 @@
 #include "ListaEncadeadaHospede.c"
 
-typedef void (*OperacaoGestaoHospede)();
+typedef void (*OperacaoGestaoHospede)(No *no);
 
 typedef struct
 {
-    int id;
+    char titulo[50];
     char descricao[256];
     OperacaoGestaoHospede operacao;
 } Opcao;
 
-void cliCadastrarUsuario(No *no)
+No *cadastrarHospede(No *no)
 {
     char nome[50];
     int pesoEmGramasAoEntrar;
@@ -26,13 +26,18 @@ void cliCadastrarUsuario(No *no)
 
     printf("Qtde dias tratamento: ");
     scanf("%d", &qtdeDiasTratamento);
+    while(qtdeDiasTratamento > 20)
+    {
+        printf("O tempo maximo de hospedagem e 20 dias: ");
+        scanf("%d", &qtdeDiasTratamento);
+    }
 
     printf(
         "Informe abaixo a qtde de calorias que deve ser \ningerida para cada dia do tratamento (%d dias):\n", 
         qtdeDiasTratamento
     );
     int valor;
-    int *listaQtdeCaloriasIngeridasPorDia;
+    int *listaQtdeCaloriasIngeridasPorDia = (int*) malloc(sizeof(int) * qtdeDiasTratamento);
     for (int i = 0; i < qtdeDiasTratamento; i++)
     {
         printf("- dia %d: ", i+1);
@@ -40,8 +45,10 @@ void cliCadastrarUsuario(No *no)
         listaQtdeCaloriasIngeridasPorDia[i] = valor;
     }
 
+    No *noRetorno;
     if (no == NULL)
-        no = noInicializar(
+    {
+        noRetorno = noInicializar(
             1,
             hospedeInicializar(
                 nome,
@@ -50,7 +57,10 @@ void cliCadastrarUsuario(No *no)
                 listaQtdeCaloriasIngeridasPorDia
             )
         );
+        return noRetorno;
+    }
     else
+    {
         noAdicionarAoFinal(
             no,
             hospedeInicializar(
@@ -60,12 +70,32 @@ void cliCadastrarUsuario(No *no)
                 listaQtdeCaloriasIngeridasPorDia
             )
         );
+        return no;
+    }
 }
 
-void cliMenu()
+void menu(No *no)
 {
+    system("cls");
+    int opcao;
     printf(
         "AMBIENTE GESTAO DE HOSPEDES - SPA REI MOMO\n\n"
         "Selecione o que deseja fazer:\n");
-    
+    printf(
+        "1. Cadastrar\n"
+        "2. Dar alta\n"
+        "3. Exibir todos\n"
+    );
+    scanf("%d", &opcao);
+    getchar();
+
+    system("cls");
+    if (opcao == 1)
+        no = cadastrarHospede(no);
+    else if (opcao == 3)
+        noImprimir(no);
+    printf("\n\nPressione [ENTER] para prosseguir");
+    getchar();
+
+    menu(no);
 }
